1a5ba853ac759645a1352610fe4fef2c1b03dcaf
Gee-Wey Yue <geewey@gmail.com>
2019-09-22 14:09:56 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,7 +1,7 @@
 def roll_call_dwarves(array)# code an argument here
   # Your code here
-  array.each_with_index do |name|
-    puts "1. #{name}"
+  array.each_with_index do |name, idx|
+    puts "#{idx}. #{name}"
   end
 end
 
